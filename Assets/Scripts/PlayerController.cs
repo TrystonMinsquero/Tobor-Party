aafecb65fe296cffde69529c52a8decb426fc293
Tryston Minsquero <tam190005@utdallas.edu>
2022-02-18 14:56:45 -0600

    fix test scene

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,9 +7,14 @@ public class PlayerController : MonoBehaviour
     private Vector2 movementInput;
     private Controls controls;
 
+    private PlayerController _playerController;
+
     private void Awake()
     {
         controls = new Controls();
+
+        _playerController = GetComponent<PlayerController>();
+        
     }
 
     private void FixedUpdate()
