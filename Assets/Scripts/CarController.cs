dbe3eb2c3e7eac7220b41bb6aedfd6dcfbc1229f
Tryston Minsquero <tam190005@utdallas.edu>
2022-02-19 22:32:12 -0600

    lobby & controls update

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -6,8 +6,15 @@ public class CarController : PlayerController
 {
     public float SteerInput { get; private set; }
     public float GasBreakInput { get; private set; }
+    
+    public bool DriftInput { get; private set; }
+    
+    public Vector2 LookInput { get; private set; }
+
 
     public void OnSteer(InputAction.CallbackContext ctx) { SteerInput = ctx.ReadValue<float>(); }
     public void OnGasBreak(InputAction.CallbackContext ctx) { GasBreakInput = ctx.ReadValue<float>(); }
+    public void OnDrift(InputAction.CallbackContext ctx) { DriftInput = ctx.ReadValue<float>() > 0.1f; }
+    public void OnLook(InputAction.CallbackContext ctx) { LookInput = ctx.ReadValue<Vector2>(); }
 
 }
