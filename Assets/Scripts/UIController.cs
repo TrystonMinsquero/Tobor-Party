791abcfc1deb6386940b18b0ac2efcd37084b2e9
Tryston Minsquero <tam190005@utdallas.edu>
2022-02-20 20:25:16 -0600

    Added UI controls

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -2,7 +2,14 @@ using UnityEngine.InputSystem;
 
 public class UIController : PlayerController
 {
+    public bool Ready { get; private set; }
+    public bool Leave { get; private set; }
+    public float ChangeSkin { private set; get; }
     public float Rotate { get; private set; }
 
+    
+    public void OnReady(InputAction.CallbackContext ctx) { Ready = ctx.ReadValue<float>() > 0.1f; }
+    public void OnLeave(InputAction.CallbackContext ctx) { Leave = ctx.ReadValue<float>() > 0.1f; }
+    public void OnChangeSkin(InputAction.CallbackContext ctx) { ChangeSkin = ctx.started ? ctx.ReadValue<float>() : 0; }
     public void OnRotate(InputAction.CallbackContext ctx) { Rotate = ctx.ReadValue<float>(); }
 }
