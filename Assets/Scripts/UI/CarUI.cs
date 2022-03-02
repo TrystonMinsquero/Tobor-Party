959adfbae3ae01f967ac7cd8476e5b46d6af25ec
Henry <htwerner01@gmail.com>
2022-03-02 00:14:23 -0600

    Made the UI punchy

--- Assets/Scripts/UI/CarUI.cs
+++ Assets/Scripts/UI/CarUI.cs
@@ -113,7 +113,7 @@ public class CarUI : MonoBehaviour
         rot += rb.velocity.magnitude * speedintervalsMultiplyer;
         
         Quaternion target = Quaternion.Euler(0, 0, rot);
-        speedometerNeedle.localRotation = Quaternion.Slerp(speedometerNeedle.localRotation, target,  Time.deltaTime * 2);
+        speedometerNeedle.localRotation = Quaternion.Slerp(speedometerNeedle.localRotation, target,  Time.deltaTime * 10);
     }
 
     public static string GetMillisecondTime(TimeSpan time)
--- Assets/Scripts/UI/PositionNumberHandler.cs
+++ Assets/Scripts/UI/PositionNumberHandler.cs
@@ -8,16 +8,69 @@ public class PositionNumberHandler : MonoBehaviour
     public Gradient placeGradient;
     public Sprite[] posNumImgs;
     private Image currentPosImg;
+    public float addedGrowthSize;
+    private int currentPos = 0;
     
 
     void Start()
     {
-        currentPosImg = this.GetComponent<Image>();
+        currentPosImg = GetComponent<Image>();
     }
 
     public void UpdatePosition(int pos)
     {
+        if (currentPos == pos) return;
+
+        currentPos = pos;
+        StartCoroutine(GrowthAnim(pos));
+    }
+
+    private IEnumerator GrowthAnim(int pos)
+    {
+        print("Growth called");
+        transform.localScale = new Vector3(1, 1, 1);
+
+        // play animation
+        float lerpTime = 0, lerpDur = 0.15f;
+        while (lerpTime < lerpDur)
+        {
+            float t = lerpTime / lerpDur;
+            t = t * t * (3f - 2f * t);
+
+            float y = Mathf.Lerp(0, addedGrowthSize, t);
+            transform.localScale = new Vector3(1 + y, 1 + y * 0.8f, 1);
+            
+            lerpTime += Time.deltaTime;
+            yield return null;
+        }
+        
+        transform.localScale = new Vector3(1, 1 + addedGrowthSize, 1);
+        
         currentPosImg.sprite = posNumImgs[pos - 1];
         currentPosImg.color = placeGradient.Evaluate((pos - 1f) / 7f);
+
+        StartCoroutine(ShrinkAnim());
+    }
+    
+    private IEnumerator ShrinkAnim()
+    {
+        print("Shrink called");
+        transform.localScale = new Vector3(1, 1 + addedGrowthSize, 1);
+
+        // play animation
+        float lerpTime = 0, lerpDur = 0.15f;
+        while (lerpTime < lerpDur)
+        {
+            float t = lerpTime / lerpDur;
+            t = t * t * (3f - 2f * t);
+
+            float y = Mathf.Lerp(addedGrowthSize, 0, t);
+            transform.localScale = new Vector3(1 + y, 1 + y * 0.8f, 1);
+            
+            lerpTime += Time.deltaTime;
+            yield return null;
+        }
+        
+        transform.localScale = new Vector3(1, 1, 1);
     }
 }
